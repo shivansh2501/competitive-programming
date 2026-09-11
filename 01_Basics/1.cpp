df97c96e367ff612d5a895e31b4b4c06ccb87db9
Shivansh Bansal <sbansal.official@gmail.com>
2026-09-11 20:12:36 +0530

    Finalizing Folder

---
+++ 01_Basics/1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello " << " " << "My name is Luv";
+    // cout << sqrt(16);
+
+    int a = 2;
+    int b = 3;
+    int sum = a + b;
+}
---
+++ 01_Basics/datatypes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // char, int, float, double, bool
+    // long int, long long int
+
+    // char c = 'a';
+    // int a = 3;
+    // double b = 3.5;
+    // a = 4.5;
+    // bool d = true;
+    // d = 123;
+    // cout << c << " " << a << " " << d << endl;
+
+    long long a = 12345;
+
+    cout << a;
+}
