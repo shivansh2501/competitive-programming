8577539d7954ec80a9fc464f66d94a5c80324298
Shivansh Bansal <sbansal.official@gmail.com>
2026-09-13 02:06:48 +0530

    Learning Operators and Datatypes

---
+++ 01_Basics/operators.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // cout << 2*3 << endl;
+
+    // cout << 25 % 7 << endl;
+
+    // int a = 5;
+    // a = a + 1;
+    // cout << a << endl;
+    // cout << a++ << endl;
+    // cout << a << endl;
+    // cout << ++a << endl;
+    // ++a;
+    // a--;
+    // --a;
+
+    // char c = 'a';
+    // char b = 'c';
+
+    // cout << (int) c << endl;
+    // cout << c - b << endl;
+
+    // int a;
+    // double b;
+    // char c;
+    // cin >> a >> b >> c;
+    // cout << a << " " << b << " " << c << " " << (int) c << endl;
+    // cout << a << "\n";
+
+    int a, b, c;
+    char d;
+    cin >> d >> a;
+    cout << d << " " << a << endl;
+
+}
